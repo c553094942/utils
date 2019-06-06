2eca8101ac9520e57ce04d7dce26672ef344adba
c553094942 <48315019+c553094942@users.noreply.github.com>
2019-06-06 14:35:13 +0800

    Create seq2jpg_caltech.py

---
+++ seq2jpg_caltech.py
@@ -0,0 +1,60 @@
+# -*- coding:utf-8 -*-
+import os.path
+import fnmatch
+import shutil
+
+
+def open_save(file, savepath):
+    # 读入一个seq文件，然后拆分成image存入savepath当中
+    f = open(file, 'rb')
+    # 将seq文件的内容转化成str类型
+    string = f.read().decode('latin-1')
+
+    # splitstring是图片的前缀，可以理解成seq是以splitstring为分隔的多个jpg合成的文件
+    splitstring = "\xFF\xD8\xFF\xE0\x00\x10\x4A\x46\x49\x46"
+    # split函数做一个测试,因此返回结果的第一个是在seq文件中是空，因此后面省略掉第一个
+    """
+    >>> a = ".12121.3223.4343"
+    >>> a.split('.')
+    ['', '12121', '3223', '4343']
+    """
+    strlist = string.split(splitstring)
+    # print(strlist)
+    # print('######################################')
+    f.close()
+    count = 0
+    # delete the image folder path if it exists
+    if os.path.exists(savepath):
+        shutil.rmtree(savepath)
+        # create the image folder path
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
+    # 遍历每一个jpg文件内容，然后加上前缀合成图片
+    for img in strlist:
+        filename = str(count) + '.jpg'
+        filenamewithpath = os.path.join(savepath, filename)
+        if count > 0:
+            i = open(filenamewithpath, 'wb+')
+            i.write(splitstring.encode('latin-1'))
+            i.write(img.encode('latin-1'))
+            i.close()
+        count = count + 1
+
+
+if __name__ == "__main__":
+    rootdir = "/media/yons/DATA/data/Caltech_origin"
+    saveroot = "/media/yons/DATA/data/Caltech/JPEGImages"
+
+    for parent, dirnames, filenames in os.walk(rootdir):
+        for filename in filenames:
+            # fnmatch 全称是 filename match，主要是用来匹配文件名是否符合规则的
+            if fnmatch.fnmatch(filename, '*.seq'):
+                thefilename = os.path.join(parent, filename)
+                # create the image folder by combining .seq file path with .seq filename
+                thesavepath = saveroot + '/' + parent.split('/')[-1] + '/' + filename.split('.')[0] + '/'
+                print("Filename=" + thefilename)
+                print("Savepath=" + thesavepath)
+                open_save(thefilename, thesavepath)
+
+# rootdir = "/media/yons/b8e254cc-f5ed-4d0d-8c4b-f47034e56b95/data/Caltech_origin
+# saveroot = "/media/yons/b8e254cc-f5ed-4d0d-8c4b-f47034e56b95/data/Caltech/JPEGImages"
